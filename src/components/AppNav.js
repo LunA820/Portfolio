e178acc1339c96383815b72fab178fa7b6248121
Luna Yuet Hei Yang <Luna.Yang@tambla.com.au>
2022-01-29 23:02:20 +1100

    update navbar

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -8,7 +8,7 @@ function AppNav() {
   return (
     <Navbar bg="dark" variant="dark" fixed="top" className="justify-content-center">
       <Nav>
-        <Nav.Link href="./" className="navItems">HOME</Nav.Link>
+        <Nav.Link href="#homePage" className="navItems">HOME</Nav.Link>
         <Nav.Link href="#aboutPage" className="navItems">ABOUT</Nav.Link>
         <Nav.Link href="#projectPage" className="navItems">PROJECTS</Nav.Link>
         <Nav.Link href="mailto:lunayang820@gmail.com" className="navItems">CONTACT</Nav.Link>
